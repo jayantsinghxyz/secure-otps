cd436dd5889ccdb99383bdebb689388abe9c4e72
Jayant Singh <jayantsinghxyz@gmail.com>
2023-03-02 20:57:39 +0530

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,9 @@ export default class SecureOtps {
   constructor(private readonly length: number) {
     this.min = parseInt("0".padEnd(length, "0"));
     this.max = parseInt("9".padEnd(length, "9"));
-    console.log(this.min, this.max);
   }
 
   public generate(): string {
     return randomInt(this.min, this.max).toString();
   }
 }
-
-console.log("new SecureOtps(4).generate()", new SecureOtps(1).generate());
